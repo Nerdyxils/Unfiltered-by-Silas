029f538c037549690b096cd0eb5fd093d67d2049
Nerdyxils <silasabiodun25@gmail.com>
2025-07-15 18:59:14 -0400

    fixed compilation error

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UnfilteredLandingPage from './components/UnfilteredLandingPage.jsx';
-import SuccessPage from './components/SuccessPage.jsx';
+// import SuccessPage from './components/SuccessPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<UnfilteredLandingPage />} />
-        <Route path="/success" element={<SuccessPage />} />
+        {/* <Route path="/success" element={<SuccessPage />} /> */}
       </Routes>
     </BrowserRouter>
   </StrictMode>,
